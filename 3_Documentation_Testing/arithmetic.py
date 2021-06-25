6323b97d5e08adbdd4fa44083a73c29d2195e730
Peter Molnar <peter@kingmolnar.com>
2021-06-25 12:29:11 -0400

    initial commit

---
+++ 3_Documentation_Testing/arithmetic.py
@@ -0,0 +1,24 @@
+"""
+Add two numbers together.
+
+>>> add(1,2)
+3
+"""
+def add (num1, num2):
+	return num1 + num2
+
+def mult(num1, num2):
+	"""
+	Multiply two numbers.
+
+	Parameters:
+		num1 -- number
+		num2 -- number
+
+	Returns:
+		product of the numbers
+
+	>>> mult(4,7)
+	28
+	"""
+	return num1 * num2
