a5e7b8af0961ae6f4022596fe5425fb7186d3b2b
Peter Molnar <pmolnar@gsu.edu>
2022-04-30 13:56:57 -0400

    from cluster

--- 1_Coding_Practices/my_utils.py
+++ 1_Coding_Practices/my_utils.py
@@ -4,9 +4,11 @@ import pandas as pd
 # example of function currying
 # https://python-course.eu/advanced-python/currying-in-python.php
 
-def get_shape_func(DATA_DIR):
+def get_shape_func(DATA_DIR, print_messages=False):
 
     def get_shape(fp):
+        if print_messages:
+            print(f"Processing file: {fp}")
         df = pd.read_csv(os.path.join(DATA_DIR, fp))
         return fp, *df.shape
 
