33a2585c535568fecb9b79f426efbd78802baa36
Peter Molnar <peter@kingmolnar.com>
2022-04-30 11:15:07 -0400

    new

---
+++ 1_Coding_Practices/my_utils.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+# example of function currying
+# https://python-course.eu/advanced-python/currying-in-python.php
+
+def get_shape_func(DATA_DIR):
+
+    def get_shape(fp):
+        df = pd.read_csv(os.path.join(DATA_DIR, fp))
+        return fp, *df.shape
+
+    return get_shape
+
+
